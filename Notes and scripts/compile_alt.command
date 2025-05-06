999f7707f20c5b3c723f4a5f6bd544c1acff2a20
Liam <kelley@bu.edu>
2025-05-06 13:09:38 -0400

    Changed ESC to GESC (grave + Esc) to make Tilde entry easier

---
+++ Notes and scripts/compile_alt.command
@@ -0,0 +1,3 @@
+cd ~/Documents/GitHub/qmk_firmware
+make massdrop/alt:lpk
+cp massdrop_alt_lpk.bin ~/Library/CloudStorage/Dropbox-Personal/Stuff/massdrop_alt_lpk.bin
--- keyboards/massdrop/alt/keymaps/lpk/keymap.c
+++ keyboards/massdrop/alt/keymaps/lpk/keymap.c
@@ -40,7 +40,7 @@ keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,          KC_UP,   KC_PGDN, \
@@ -399,9 +399,9 @@ led_instruction_t led_game_instructions_c2077[] = {
 };
 
 void *led_instruction_list[] = {
+    led_instructions_white_teal,
     led_instructions_green_teal_and_purple,
     led_instructions_blue_teal_and_pink,
-    led_instructions_white_teal,
     led_instructions_gradient,
     led_instructions_c2077,
     led_instructions_white_mains,
@@ -409,9 +409,9 @@ void *led_instruction_list[] = {
 };
 
 void *led_game_instruction_list[] = {
+    led_game_instructions_white_teal,
     led_game_instructions_green_teal_and_purple,
     led_game_instructions_blue_teal_and_pink,
-    led_game_instructions_white_teal,
     led_game_instructions_white_teal,
     led_game_instructions_c2077,
     led_game_instructions_white_mains,
